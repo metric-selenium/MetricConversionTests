f597672e94f564bec7675018637cc5a2c041449f
masmangan <marco.mangan@gmail.com>
2024-07-27 22:45:18 -0300

    Local testing running

---
+++ MetricConversionTests/CelsiusToFahrenheitTest.cs
@@ -0,0 +1,51 @@
+namespace MetricConversionTests;
+
+using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+
+public class CelsiusToFahrenheitTest : IDisposable
+{
+    IWebDriver driver;
+    WebDriverWait wait;
+
+
+    public CelsiusToFahrenheitTest()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+    }
+
+    [Theory]
+    [InlineData("100", "212")]
+    public void TestCelsiusToFahrenheitConversion(string c, string f)
+    {
+        // Navigate to the target webpage
+        driver.Navigate().GoToUrl("https://www.metric-conversions.org/temperature/celsius-to-fahrenheit.htm#google_vignette");
+
+        // Locate the Celsius input field
+        IWebElement celsiusInputField = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='arg']")));
+
+        // Input a value into the Celsius field
+        celsiusInputField.Clear();
+        celsiusInputField.SendKeys(c);
+
+        // Locate the Fahrenheit result field (it may update automatically)
+        IWebElement fahrenheitResultField = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='answerDisplay']")));
+
+        // Verify the Fahrenheit result
+        string result = fahrenheitResultField.Text;
+        Assert.Contains(f, result);
+    }
+
+    public void Dispose()
+    {
+        driver.Quit();
+    }
+}
